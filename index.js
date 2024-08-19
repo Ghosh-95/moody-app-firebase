9c4b22021120be81c833e88fe98309645609f559
Sushovan <sushovanghosh960@gmail.com>
2024-08-19 12:22:39 +0530

    feat: imlement sign in with google and update profile feature

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // imports
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, updateProfile } from "firebase/auth";
 import { firebaseConfig } from "./firebase-config";
 
 // Initialize Firebase
@@ -8,6 +8,8 @@ const app = initializeApp(firebaseConfig);
 
 // Initilize Firebase auth
 const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
 
 /* === UI === */
 
@@ -26,7 +28,19 @@ const createAccountButtonEl = document.getElementById("create-account-btn")
 
 const signOutButtonEl = document.querySelector(".js-logout-btn");
 
+const userProfilePictureEl = document.getElementById("user-profile-picture");
+const userGreetingEl = document.getElementById("user-greet");
+
+const profielUpdateFormEl = document.getElementById("profile-update-form");
+const displayNameInputEl = document.getElementById("display-name-input");
+const photoUrlInputEl = document.getElementById("photo-url-input");
+const updateProfileButtonEl = document.getElementById("update-profile-btn");
+
+const toggleUpdateFormButtonEl = document.getElementById("toggle-update-form-btn");
+
 /* == UI - Event Listeners == */
+
+toggleUpdateFormButtonEl.addEventListener("click", toggleUpdateProfileForm);
 
 signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle)
 
@@ -35,13 +49,16 @@ createAccountButtonEl.addEventListener("click", authCreateAccountWithEmailAndPas
 
 signOutButtonEl.addEventListener("click", authSignOut);
 
+updateProfileButtonEl.addEventListener("click", authUpdateProfile);
+
 // variables
 
 /* === Main Code === */
 onAuthStateChanged(auth, user => {
     if (user) {
-        const uID = user.uid;
         showLoggedInView();
+        showProfilePicture(userProfilePictureEl, user);
+        showUserGreeting(userGreetingEl, user);
     } else {
         showLoggedOutView();
     }
@@ -53,7 +70,13 @@ onAuthStateChanged(auth, user => {
 /* = Functions - Firebase - Authentication = */
 
 function authSignInWithGoogle() {
-    console.log("Sign in with Google")
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            console.log("Signed in with google");
+
+        }).catch((error) => {
+            console.error(error.message);
+        });
 }
 
 function authSignInWithEmailAndPassword() {
@@ -90,9 +113,28 @@ function authCreateAccountWithEmailAndPassword() {
 
 function authSignOut() {
     signOut(auth).then(() => {
-        console.log("Sign out");
-
     }).catch(err => console.error(err))
+};
+
+function authUpdateProfile(e) {
+    e.preventDefault();
+
+    const newDisplayName = displayNameInputEl.value;
+    const newPhotoUrl = photoUrlInputEl.value;
+
+    updateProfile(auth.currentUser, {
+        displayName: newDisplayName,
+        photoURL: newPhotoUrl,
+    })
+        .then(() => {
+            console.log("Profile Updated");
+            clearUpdateInput();
+        })
+        .catch(err => console.error(err))
+
+    toggleUpdateFormButtonEl.classList.remove("hide");
+    profielUpdateFormEl.classList.add("hide");
+
 };
 
 /* == Functions - UI Functions == */
@@ -122,4 +164,34 @@ function clearInputField(field) {
 function clearAuthInput() {
     clearInputField(emailInputEl);
     clearInputField(passwordInputEl);
+};
+
+function clearUpdateInput() {
+    clearInputField(displayNameInputEl);
+    clearInputField(photoUrlInputEl);
+}
+
+function showProfilePicture(fieldElement, user) {
+    const photoURL = user.photoURL;
+
+    if (photoURL) {
+        fieldElement.src = photoURL;
+    } else {
+        fieldElement.src = "./assets/default-profile-picture.svg"
+    };
+};
+
+function showUserGreeting(elem, user) {
+    const userName = user.displayName;
+
+    if (userName) {
+        elem.textContent = "Hi " + userName.split(" ").at(0);
+    } else {
+        elem.textContent = "Hello buddy!";
+    }
+};
+
+function toggleUpdateProfileForm(e) {
+    profielUpdateFormEl.classList.remove("hide");
+    e.target.classList.add("hide");
 }
